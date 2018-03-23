d030e914deba39e269fd4e3276a813f5a6c369c2
Jordan Levin <jlevin38@binghamton.edu>
2018-03-23 14:35:13 -0400

    ass3 finished

--- ass3/main.cpp
+++ ass3/main.cpp
@@ -8,166 +8,114 @@
 #include <ctime>
 #include <iostream>
 
-float rot = 0;
-float ex = 0, ey = -40, ez = 0, cx = 0, cy = 0, cz = 0, ux = 0, uy = 0, uz = 1;
-float crotx = 0, croty = 0, crotz = 0;
-bool keys[255];
+#define ANGLE 1
+#define RADIANS (M_PI/180)
 
 struct point{
-    float x, y;
-    point() = default;
-    point(float xp, float yp){
-        x = xp;
-        y = yp;
+    float x, y, z;
+    point(float px, float py, float pz){
+        x = px; y = py; z = pz;
     }
 };
+GLUquadricObj* quadric;
+int width, height;
+point cam(0, 0, 40);
+point n(0,0,-1);
+point u(0,1,0);
+point v(1,0,0);
+//camera rotations
+float crotx = 0, croty = 0, crotz = 0;
+//lever rotations
+float rotlever = 0;
 
-point rotate(point origin, point p, float angle){
-    float s = std::sin(angle);
-    float c = std::cos(angle);
-
-    float xmin = std::min(origin.x, p.x);
-    float ymin = std::min(origin.y, p.y);
-    
-    //this function doesn't work for negatives
-    //this hack fixes it
-    if(xmin < 0){
-        p.x -= xmin;
-        origin.x -= xmin;
-    }
-    if(ymin < 0){
-        p.y -= ymin;
-        origin.y -= ymin;
-    }
-
-    p.x -= origin.x;
-    p.y -= origin.y;
-
-    p.x = p.x*c - p.y*s + origin.x;
-    p.y = p.x*s + p.y*c + origin.y;
-
-    //undo the hack from earlier after calculations are done
-    if(xmin < 0)
-        p.x += xmin;
-    if(ymin < 0)
-        p.y += ymin;
-
-    return p;
-}
+bool keys[255];
 
 void key(unsigned char key, int x, int y){
-    printf("ex: %f, ey: %f, ez: %f, cx: %f, cy: %f, cz: %f, ux: %f, uy: %f, uz: %f\n",
-            ex, ey, ez, cx, cy, cz, ux, uy, uz);
+    //printf("ex: %f, ey: %f, ez: %f, cx: %f, cy: %f, cz: %f, ux: %f, uy: %f, uz: %f\n",
+            //cam.x, cam.y, cam.z, cam.x+n.x, cam.y+n.y, cam.z+n.z, u.x, u.y, u.z);
     keys[key] = true;
 }
 void key_up(unsigned char key, int x, int y){
     keys[key] = false;
 }
 
-
-void pitch_up(){
-    crotx += 0.01;
-    point up = rotate(point(0, 0), point(uy, uz), 0.01);
-    uy = up.x;
-    uz = up.y;
-    point center = rotate(point(ey, ez), point(cy, cz), 0.01);
-    cy = center.x;
-    cz = center.y;
+void roll(float angle){
+    croty += angle;
+    float ux_t = u.x;
+    u.x = std::cos(angle*RADIANS)*ux_t - std::sin(angle*RADIANS)*u.y;
+    u.y = std::sin(angle*RADIANS)*ux_t + std::cos(angle*RADIANS)*u.y;
+    float vx_t = v.x;
+    v.x = std::cos(angle*RADIANS)*vx_t - std::sin(angle*RADIANS)*v.y;
+    v.y = std::sin(angle*RADIANS)*vx_t + std::cos(angle*RADIANS)*v.y;
 }
 
-void pitch_down(){
-    crotx -= 0.01;
-    point up = rotate(point(0, 0), point(uy, uz), -0.01);
-    uy = up.x;
-    uz = up.y;
-    point center = rotate(point(ey, ez), point(cy, cz), -0.01);
-    cy = center.x;
-    cz = center.y;
-
+void yaw(float angle){
+    crotz += angle;
+    //rotate center
+    float nx_t = n.x;
+    n.x = std::cos(angle*RADIANS)*nx_t - std::sin(angle*RADIANS)*n.z;
+    n.z = std::sin(angle*RADIANS)*nx_t + std::cos(angle*RADIANS)*n.z;
+    //rotate side
+    float vx_t = v.x;
+    v.x = std::cos(angle*RADIANS)*vx_t - std::sin(angle*RADIANS)*v.z;
+    v.z = std::sin(angle*RADIANS)*vx_t + std::cos(angle*RADIANS)*v.z;
 }
 
-void yaw_right(){
-    crotz -= 0.01;
-    float cxrel, cyrel;
-    cxrel = (cx - ex);
-    cyrel = (cy - ey);
-    point center = rotate(point(0, 0), point(cxrel, cyrel), -0.01);
-    cx = center.x + ex;
-    cy = center.y + ey;
+void pitch(float angle){
+    crotx -= angle;
+    //rotate center
+    float nz_t = n.z;
+    n.z = std::cos(angle*RADIANS)*nz_t - std::sin(angle*RADIANS)*n.y;
+    n.y = std::sin(angle*RADIANS)*nz_t + std::cos(angle*RADIANS)*n.y;
+    //rotate up
+    float uz_t = u.z;
+    u.z = std::cos(angle*RADIANS)*uz_t - std::sin(angle*RADIANS)*u.y;
+    u.y = std::sin(angle*RADIANS)*uz_t + std::cos(angle*RADIANS)*u.y;
 }
 
-void yaw_left(){
-    crotz += 0.01;
-    point center = rotate(point(ex, ey), point(cx, cy), 0.01);
-    cx = center.x;
-    cy = center.y;
+void slide_forward(float dist){
+    cam.x += dist*n.x;
+    cam.y += dist*n.y;
+    cam.z += dist*n.z;
+}
+void slide_right(float dist){
+    cam.x += dist*v.x;
+    cam.y += dist*v.y;
+    cam.z += dist*v.z;
+}
+void slide_up(float dist){
+    cam.x += dist*u.x;
+    cam.y += dist*u.y;
+    cam.z += dist*u.z;
 }
 
-void roll_right(){
-    croty -= 0.01;
-    ux = std::cos(croty);
-    uz = std::sin(croty);
-}
-
-void roll_left(){
-    croty += 0.01;
-    ux = std::cos(croty + M_PI/4);
-    uz = std::sin(croty + M_PI/4);
-}
-
-//movement
-void slide_forward(){
-    ex += std::cos(crotx);
-    ez += std::sin(crotx);
-
-    ex += std::sin(crotz);
-    ey += std::cos(crotz);
-
-    cx += std::cos(crotx);
-    cz += std::sin(crotx);
-
-    cx += std::sin(crotz);
-    cy += std::cos(crotz);
-
-}
-
-void slide_backward(){
-    ey -= 0.1;
-    cy -= 0.1;
-}
-//temp
-void strafe_right(){
-    ex += 0.1;
-    cx += 0.1;
-}
-
-void strafe_left(){
-    ex -= 0.1;
-    cx -= 0.1;
-}
-
-void physics(){
+void camera(){
     if(keys['q'])
-        roll_left();
+        roll(ANGLE);
     if(keys['e'])
-        roll_right();
+        roll(-ANGLE);
     if(keys['w'])
-        pitch_down();
+        pitch(ANGLE);
     if(keys['s'])
-        pitch_up();
-    if(keys['d'])
-        yaw_right();
+        pitch(-ANGLE);
     if(keys['a'])
-        yaw_left();
-    //temp
+        yaw(-ANGLE);
+    if(keys['d'])
+        yaw(ANGLE);
     if(keys['i'])
-        slide_forward();
+        slide_forward(ANGLE);
     if(keys['k'])
-        slide_backward();
-    if(keys['l'])
-        strafe_right();
+        slide_forward(-ANGLE);
+    if(keys['o'])
+        slide_up(ANGLE);
+    if(keys['u'])
+        slide_up(-ANGLE);
     if(keys['j'])
-        strafe_left();
+        slide_right(-ANGLE);
+    if(keys['l'])
+        slide_right(ANGLE);
+    if(keys['r'])
+        rotlever += 1;
 }
 
 void draw_ground(){
@@ -190,29 +138,109 @@ void draw_ground(){
     glPopMatrix();
 }
 
+void viewport(int width, int height){
+  float ratio = (float)width/height;
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(60, ratio, 1, 256);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void draw_lever(){
+    glColor3f(1, 0, 0);
+    glTranslatef(0, -5, 0);
+    
+    //main cylinder
+    glPushMatrix();
+    glRotatef(90,1,0,0);
+    gluCylinder(quadric, 1, 1, 10, 10, 10);
+    glPopMatrix();
+    
+    //center sphere
+    glRotatef(rotlever, 0, 1, 0);
+    glutSolidSphere(1, 10, 10);
+    glPushMatrix();
+    
+    //top center cylinder
+    glTranslatef(0, 0, -10);
+    gluCylinder(quadric, 1, 1, 20, 10, 10);
+
+    //right side sphere
+    glRotatef(rotlever, 0, 0, 1);
+    glutSolidSphere(1, 10, 10);
+
+    //rest of right side
+    glTranslatef(0, -5, 0);
+    glRotatef(-90, 1, 0, 0);
+    gluCylinder(quadric, 1, 0, 5, 10, 10);
+    glutSolidSphere(1.5, 10, 10);
+    glPopMatrix();
+
+    //left side sphere
+    glTranslatef(0, 0, 10);
+    glRotatef(-rotlever, 0, 0, 1);
+    glutSolidSphere(1, 10, 10);
+    
+    //the rest of left side
+    glTranslatef(0, -5, 0);
+    glRotatef(-90, 1, 0, 0);
+    gluCylinder(quadric, 1, 0, 5, 10, 10);
+    glutSolidSphere(1.5, 10, 10);
+
+
+    glColor3f(1, 1, 1);
+}
+
+void draw_viewports(){
+    //Viewport 1
+    glViewport(width/2.0, 0, width/2.0, height/2.0);
+    viewport(width/2.0, height/2.0);
+    glPushMatrix();
+    gluLookAt(cam.x, cam.y, cam.z, cam.x+n.x, cam.y+n.y, cam.z+n.z, u.x, u.y, u.z);
+    draw_ground();
+    draw_lever();
+    glPopMatrix();
+    //Viewport top
+    glViewport(0, 0, width/2.0, height/2.0);
+    viewport(width/2.0, height/2.0);
+    glPushMatrix();
+    gluLookAt(0, 40, 0, 0, 0, 0, 1, 0, 0);
+    draw_ground();
+    draw_lever();
+    glPopMatrix();
+    //Viewport side
+    glViewport(width/2.0, height/2.0, width/2.0, height/2.0);
+    viewport(width/2.0, height/2.0);
+    glPushMatrix();
+    gluLookAt(40, 0, 0, 0, 0, 0, 0, 1, 0);
+    draw_ground();
+    draw_lever();
+    glPopMatrix();
+    //Viewport front
+    glViewport(0, height/2.0, width/2.0, height/2.0);
+    viewport(width/2.0, height/2.0);
+    glPushMatrix();
+    gluLookAt(0, 0, -40, 0, 0, 0, 0, 1, 0);
+    draw_ground();
+    draw_lever();
+    glPopMatrix();
+
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-    
-    physics();
 
-    gluLookAt(ex, ey, ez, cx, cy, cz, ux, uy, uz);
-    draw_ground();
-
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glTranslatef(cx, cy, cz);
-    glutSolidCube(1);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glTranslatef(-cx, -cy, -cz);
-    glTranslatef(1,8,0);
-    glutWireCube(2);
-    glTranslatef(-2,1,1);
-    glutWireCube(1);
-    glTranslatef(-2,-3,-1);
-    glutWireCube(1);
+    //process camera controls
+    camera();
+    //draw all the viewports
+    draw_viewports();   
 }
 
 void reshape(int w, int h) {
+    width = w;
+    height = h;
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -238,6 +266,8 @@ int main(int argc, char** argv) {
     glutReshapeFunc(reshape);
     glutKeyboardFunc(key);
     glutKeyboardUpFunc(key_up);
+    quadric = gluNewQuadric();
+    gluQuadricDrawStyle(quadric, GLU_LINE);
     timer(0);
     glutMainLoop();
     return EXIT_SUCCESS;
