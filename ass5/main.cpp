c88ec9f9c5c1558a2418712ca028b1142a23d792
Jordan Levin <jlevin38@binghamton.edu>
2018-04-23 17:45:55 -0400

    assignment 5 bezier and backface culling done, starting lighting

--- ass5/main.cpp
+++ ass5/main.cpp
@@ -1,59 +1,19 @@
 #include <stdlib.h>
-
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <cmath>
 #include <cstdlib>
-#include <ctime>
 #include <iostream>
 #include <array>
-
-
-struct Point{
-    float x=0, y=0, z=0;
-    Point() = default;
-    Point(float x, float y, float z){
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
-};
-
-std::array<std::array<Point, 4>, 4> controls;
-std::array<std::array<Point, 16>, 16> points;
-int WIDTH;
-int HEIGHT;
-bool clicked = false;
-Point* curr;
-
-bool keys[255];
+#include <vector>
+#include "utils.h"
 
 void mouse(int button, int state, int x, int y){
-    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && !clicked){
-        double obj_x, obj_y, obj_z;
-        GLint view[4];
-        GLdouble proj[16];
-        GLdouble model[16];
-
-        glGetDoublev(GL_MODELVIEW_MATRIX, model);
-        glGetDoublev(GL_PROJECTION_MATRIX, proj);
-        glGetIntegerv( GL_VIEWPORT, view);
-        gluUnProject(0, 0, y, model, proj, view, &obj_x, &obj_y, &obj_z);
-        printf("x: %f, y: %f, z: %f\n", obj_x, obj_y, obj_z);
-
-        for(auto& row: controls){
-            for(Point& p: row){
-                if(std::abs(p.x - obj_x) < .5 && std::abs(p.y - obj_y) < .5){
-                    curr = &p;
-                    clicked = true;
-                }
-            }
-        }
-    }
-    else if(clicked){
-        clicked = false;
-    }
+    unsigned char pixel[3];
+    glReadPixels(x,glutGet(GLUT_WINDOW_HEIGHT) - y,
+        1,1,GL_RGB, GL_UNSIGNED_BYTE, &pixel);
+    curr = &(controls[pixel[0]/85][pixel[2]/85]);
 }
 
 void move(int x, int y){
@@ -65,8 +25,21 @@ void move(int x, int y){
 }
 
 void key(unsigned char key, int x, int y){
-    //temp
-    curr = &(controls[2][2]);
+    if(key == 'i'){
+        ambient = true;
+        diffuse = false;
+        specular = false;
+    }
+    else if(key == 'o'){
+        ambient = false;
+        diffuse = true;
+        specular = false;
+    }
+    else if(key == 'p'){
+        ambient = false;
+        diffuse = false;
+        specular = true;
+    }
     keys[key] = true;
 }
 void key_up(unsigned char key, int x, int y){
@@ -96,58 +69,67 @@ Point bezier_patch(float u, float v){
 void draw_axis(){
     glLineWidth(3);
     glBegin(GL_LINES);
-        glVertex3f(-50, 0, 0);
-        glVertex3f(50, 0, 0);
-        glVertex3f(0, -50, 0);
-        glVertex3f(0, 50, 0);
-        glVertex3f(0, 0, -50);
-        glVertex3f(0, 0, 50);
+        glVertex3f(-100, 0, 0);
+        glVertex3f(100, 0, 0);
+        glVertex3f(0, -100, 0);
+        glVertex3f(0, 100, 0);
+        glVertex3f(0, 0, -100);
+        glVertex3f(0, 0, 100);
     glEnd();
 }
 
 void draw_controls(){
-    glPointSize(10);
-    glColor3f(1,0,0);
+    glPointSize(20);
     glBegin(GL_POINTS);
-    for(auto i: controls){
-        for(Point p: i){
-            glVertex3f(p.x, p.y, p.z);
+    for (int i = 0; i < controls.size(); ++i) {
+        for (int j = 0; j < controls[0].size(); ++j) {
+            float c = 1.0/255.0;
+            glColor3f(controls[i][j].r*c, 0, controls[i][j].b*c);
+            glVertex3f(controls[i][j].x*2, controls[i][j].y*2, controls[i][j].z*2);
         }
     }
     glEnd();
     glPointSize(1);
 }
 
-void draw_points(){
-    glLineWidth(1);
-    glPointSize(5);
-    glColor3f(0,1,0);
-    glBegin(GL_POINTS);
-    for(auto i: points){
-        for(Point p: i){
-            glVertex3f(p.x, p.y, p.z);
+void make_triangles(){
+    for(int i = 0; i < points.size()-1; i++){
+        for(int j = 0; j < points[0].size()-1; j++){
+            triangles.push_back(Triangle(points[i+1][j], points[i][j+1], points[i+1][j+1]));
         }
     }
-    glEnd();
+}
+
+void draw_triangles(){
+    Point v = Point(45, 45, 45);
     glColor3f(1,1,1);
     glBegin(GL_LINES);
-    for(int i = 0; i < points.size(); i++){
-        for(int j = 0; j < points[0].size()-1; j++){
-            glVertex3f(points[i][j].x, points[i][j].y, points[i][j].z);
-            glVertex3f(points[i][j+1].x, points[i][j+1].y, points[i][j+1].z);
-        }
-    }
-    for(int i = 0; i < points.size(); i++){
-        for(int j = 0; j < points[0].size()-1; j++){
-            glVertex3f(points[j][i].x, points[j][i].y, points[j][i].z);
-            glVertex3f(points[j+1][i].x, points[j+1][i].y, points[j+1][i].z);
-        }
+    for(auto t: triangles){
+        Point n = normal(t);
+        if(dot(v, n) > 0)
+            continue;
+        glVertex3f(t.a.x*2, t.a.y*2, t.a.z*2);
+        glVertex3f(t.b.x*2, t.b.y*2, t.b.z*2);
+        glVertex3f(t.b.x*2, t.b.y*2, t.b.z*2);
+        glVertex3f(t.c.x*2, t.c.y*2, t.c.z*2);
+        glVertex3f(t.c.x*2, t.c.y*2, t.c.z*2);
+        glVertex3f(t.a.x*2, t.a.y*2, t.a.z*2);
     }
     glEnd();
-    glPointSize(1);
 }
 
-void display() {
+void calculate_bezier(){
+    for (int i = 0; i < points.size(); ++i) {
+        for (int j = 0; j < points[0].size(); ++j) {
+            //printf("%f, %f, %f\n", points[i][j].x, points[i][j].y, points[i][j].z);
+            points[i][j] = 
+                bezier_patch((float)i/19.0, (float)j/19.0);
+            //printf("After %f, %f, %f\n", points[i][j].x, points[i][j].y, points[i][j].z);
+        }
+    }
+}
+
+void display(){
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f );
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
@@ -170,22 +152,20 @@ void display() {
     gluPerspective(60, 1, 1, 500000);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(25,15,25,0,0,0,0,1,0);
-    
-    for (int i = 0; i < points.size(); ++i) {
-        for (int j = 0; j < points[0].size(); ++j) {
-            //printf("%f, %f, %f\n", points[i][j].x, points[i][j].y, points[i][j].z);
-            points[i][j] = 
-                bezier_patch((float)i/15.0, (float)j/15.0);
-            //printf("After %f, %f, %f\n", points[i][j].x, points[i][j].y, points[i][j].z);
-        }
-    }
-    
+    gluLookAt(45,45,45,0,0,0,0,1,0);
+
+    calculate_bezier();
+    triangles.clear();
+    make_triangles();
+
     glColor3f(1, 1, 1);
     draw_axis();
-    draw_points();
+    //draw_points();
+    draw_triangles();
     draw_controls();
 }
+
+
 
 void reshape(int w, int h) {
     glViewport(0, 0, w, h);
@@ -203,33 +183,6 @@ void timer(int){
     glutTimerFunc(1000/60, timer, 0);
 }
 
-void init_control_points(){
-    glEnable(GL_PROGRAM_POINT_SIZE);
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            controls[i][j].x = i*4+i;
-            controls[i][j].z = j*4+j;
-        }
-    }
-}
-
-void init_points(){
-    for (int i = 0; i < points.size(); ++i) {
-        for (int j = 0; j < points[0].size(); ++j) {
-            points[i][j].x = i;
-            points[i][j].z = j;
-        }
-    }
-}
-
-void print_points(){
-    for (int i = 0; i < points.size(); ++i) {
-        for (int j = 0; j < points[0].size(); ++j) {
-            printf("%f, %f, %f\n", points[i][j].x, points[i][j].y, points[i][j].z);
-        }
-    }
-}
-
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
@@ -244,7 +197,8 @@ int main(int argc, char** argv) {
     glutPassiveMotionFunc(move);
     glutKeyboardUpFunc(key_up);
     init_control_points();
-    init_points();
+    calculate_bezier();
+    make_triangles();
     //print_points();
     timer(0);
     glutMainLoop();
---
+++ ass5/utils.h
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <array>
+#include <vector>
+
+struct Point{
+    float x=0, y=0, z=0;
+    float r=0, g=0, b=0;
+    Point() = default;
+    Point(float x, float y, float z){
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    Point operator-(Point other){
+        return Point(x-other.x, y-other.y, z-other.z);
+    }
+};
+
+struct Triangle{
+    Point a, b, c;
+    Triangle() = default;
+    Triangle(Point p1, Point p2, Point p3){
+        a = p1;
+        b = p2;
+        c = p3;
+    }
+};
+
+std::array<std::array<Point, 4>, 4> controls;
+std::array<std::array<Point, 20>, 20> points;
+std::vector<Triangle> triangles;
+int WIDTH;
+int HEIGHT;
+bool clicked = false;
+bool ambient = false, diffuse = false, specular = false;
+Point* curr;
+
+bool keys[255];
+
+
+
+float LightAmbient[] = {0.1,0.1,0.1,1.0};
+float LightDiffuse[] = {0.7, 0.7, 0.7, 1.0};
+float LightSpecular[]= {0.4, 0.4, 0.4, 1.0};
+
+Point normal(Triangle t){
+    Point ret;
+    Point u = t.b - t.a;
+    Point v = t.c - t.a;
+    ret.x = u.y*v.z - u.z*v.y;
+    ret.y = u.z*v.x - u.x*v.z;
+    ret.z = u.x*v.y - u.y*v.x;
+    return ret;
+}
+
+int dot(Point p1, Point p2){
+    return p1.x*p2.x + p1.y*p2.y + p1.z*p2.z;
+}
+
+void init_control_points(){
+    glEnable(GL_PROGRAM_POINT_SIZE);
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            controls[i][j].r = i*85;
+            controls[i][j].b = j*85;
+            controls[i][j].x = i*4+i;
+            controls[i][j].z = j*4+j;
+        }
+    }
+}
+
+void print_triangles(){
+    for(Triangle t: triangles){
+        printf("Normal: %f, %f, %f\n", normal(t).x, normal(t).y, normal(t).z);
+        printf("%f, %f, %f\n", t.a.x, t.a.y, t.a.z);
+        printf("%f, %f, %f\n", t.b.x, t.b.y, t.b.z);
+        printf("%f, %f, %f\nend\n", t.c.x, t.c.y, t.c.z);
+    }
+}
+
+void print_points(){
+    for (int i = 0; i < points.size(); ++i) {
+        for (int j = 0; j < points[0].size(); ++j) {
+            printf("%f, %f, %f\n", points[i][j].x, points[i][j].y, points[i][j].z);
+        }
+    }
+}
+
